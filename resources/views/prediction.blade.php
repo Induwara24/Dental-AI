0929ab592210dab0c837ad9befcc422a14e287f9
Ashen Samuditha <ashensamuditha2017@gmail.com>
2025-08-06 20:14:58 +0530

    Show the grand heatmap

--- resources/views/prediction.blade.php
+++ resources/views/prediction.blade.php
@@ -19,6 +19,12 @@
             <p><strong>Predicted Class:</strong> {{ $prediction['predicted_class'] }}</p>
             <p><strong>Confidence:</strong> {{ number_format($prediction['confidence'] * 100, 2) }}%</p>
         </div>
+        @if(isset($prediction['heatmap_image']))
+            <div>
+                <h2>Model's Focus Area (Grad-CAM)</h2>
+                <img src="data:image/jpeg;base64,{{ $prediction['heatmap_image'] }}" alt="Grad-CAM Heatmap">
+            </div>
+        @endif
     @endif
 
     @if(isset($error))
